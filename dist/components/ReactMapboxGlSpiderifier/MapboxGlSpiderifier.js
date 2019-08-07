2537cc3ee2d479d8eda3f5db87033de7640a550c
tristyn-zikher <38231537+tristyn-zikher@users.noreply.github.com>
2019-08-07 10:16:57 -0500

    Update MapboxGlSpiderifier.js

--- dist/components/ReactMapboxGlSpiderifier/MapboxGlSpiderifier.js
+++ dist/components/ReactMapboxGlSpiderifier/MapboxGlSpiderifier.js
@@ -104,7 +104,7 @@ function (_Component) {
           index: index,
           transitionDelay: _this3._getTransitionDelay(props, index),
           style: {
-            zIndex: ( (index >= 5) ? (200 + (index - 5)) : (200 + index) )
+            zIndex: ( (index >= 5) ? (200 + (index - 3)) : (200 + index) )
           }
         });
       });
